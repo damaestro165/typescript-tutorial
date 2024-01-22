2a75d0099fdf44e006a67567c65fc3031dc1af62
damaestro165 <damaestro165@gmail.com>
2024-01-22 11:23:50 +0100

    Final

--- public/app.js
+++ public/app.js
@@ -1,16 +1,24 @@
+import { ListTemplate } from './classes/ListTemplate.js';
+import { Payment } from './classes/Payments.js';
 import { Invoice } from './classes/Invoice.js';
 const form = document.querySelector('.new-item-form');
 const type = document.querySelector('#type');
 const tofrom = document.querySelector('#tofrom');
 const details = document.querySelector('#details');
 const amount = document.querySelector('#amount');
-const invOne = new Invoice('mario', 'work on the mario website', 250);
-let invoices = [];
-invoices.push(invOne);
-invoices.forEach(inv => {
-    console.log(inv.client);
-});
+const ul = document.querySelector('ul');
+const list = new ListTemplate(ul);
 form.addEventListener('submit', (e) => {
     e.preventDefault();
-    console.log(type.value, tofrom.value, details.value, amount.value);
+    // tuples
+    let values;
+    values = [tofrom.value, details.value, amount.valueAsNumber];
+    let doc;
+    if (type.value === 'invoice') {
+        doc = new Invoice(...values);
+    }
+    else {
+        doc = new Payment(...values);
+    }
+    list.render(doc, type.value, 'end');
 });
